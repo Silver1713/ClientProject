5583bb8e3ec15d8b44fe4e24f05e78d19d03a6a6
Edwin <edwinleezr00@gmail.com>
2025-04-12 03:26:10 +0800

    push client main

--- src/safe_cout.cpp
+++
@@ -1,9 +0,0 @@
-#include "safe_cout.hpp"
-
-SafeCout& SafeCout::operator<<(std::ostream& (*func)(std::ostream&))
-{
-	std::lock_guard<std::mutex> lock(_stdoutMutex);
-	std::cout << func;
-	return *this;
-}
-
